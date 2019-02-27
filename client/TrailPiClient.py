b8726a9a5307d9db54a05a148b021c8517dc98d9
Brody Reeves <bareeves@ucdavis.edu>
2019-02-26 17:58:06 -0800

    Expanding on building blocks
    Fill out some of the server/client scripts
    
    <Upd> 'client/TrailPiClient.py'
    <Upd> 'server/TrailPiServer.py'

--- client/TrailPiClient.py
+++ client/TrailPiClient.py
@@ -1,4 +1,8 @@
+import sys
 import socket
+
+# FIXME just put host ip up here as global for testing ease, remove when unnecessary
+HOST_IP = '::1'
 
 # enforce version
 MIN_PYTHON = (3, 7)
@@ -8,25 +12,120 @@ if sys.version_info < MIN_PYTHON:
 # helpful link for threaded python3 ipv6 server script
 # https://www.geeksforgeeks.org/socket-programming-multi-threading-python/
 
-if __name__ == '__main__':
-    host = '2601:203:480:17d8:196d:2760:13fe:127e'
+def check_in(sock):
+    """
+    check_in: Uses the socket to check in so the server knows the camera is alive.
+    Inputs:
+        sock - the socket to use
+    """
+    # first message indicates a request to check in
+    message = 'alive'
+    sock.send(message.encode('ascii'))
+
+    sock_reply = sock.recv(4)
+    print('Received reply from server: ', sock_reply) # DEBUG debugging output
+
+    if sock_reply.upper() == 'ACK':
+        # TODO implement the actual site idenfification
+
+        print('Server ACK\'d check-in request') # DEBUG debugging output
+        message = '14' # DEBUG pretend to send site identification
+        sock.send(message.encode(ascii))
+
+        sock_reply = sock.recv(4)
+        if sock_reply.upper() == 'ACK':
+            print('Server ACK\'d site check-in') # DEBUG debugging output
+        else:
+            # TODO handle failed identification
+            print('Server didn\'t ACK site check-in') # DEBUG debugging output
+        # TODO implement the actual site identification
+    else:
+        # TODO handle failed request
+        print('Server didn\'t ACK check-in request') # DEBUG debugging output
+
+def new_image(sock, image_data):
+    """
+    new_image: Uses the socket to transfer a new image to the server.
+    Inputs:
+        sock - the socket to use
+        image_data - the data for the image to transfer
+    """
+    # first message indicates a request to transfer an image
+    message = 'image'
+    sock.send(message.encode('ascii'))
+
+    sock_reply = sock.recv(4)
+    print('Received reply from server: ', sock_reply) # DEBUG debugging output
+
+    if sock_reply.upper() == 'ACK':
+        # TODO implement the actual image transfer
+
+        print('Server ACK\'d transfer request') # DEBUG debugging output
+        message = image_data or 'No data' # DEBUG pretend to send image data
+        sock.send(message.encode('ascii'))
+
+        sock_reply = sock.recv(4)
+        if sock_reply.upper() == 'ACK':
+            print('Server ACK\'d image transfer') # DEBUG debugging output
+        else:
+            # TODO handle a failed transfer
+            print('Server didn\'t ACK image transfer') # DEBUG debugging output
+        # DEBUG pretend to send image data
+
+    else:
+        # TODO handle failed request
+        print('Server didn\'t ACK transfer request') # DEBUG debugging output
+
+def make_connection(mode, image_data = None):
+    """
+    make_connection: Establishes a connection between the client and the server then calls logic function based on mode.
+    Inputs:
+        mode - what mode the connection will be used for
+        image_data - if a new image is being transferred to the server, this will contain the appropriate data
+    Modes:
+        0: checks in so the server knows the client is active
+        1: client has a new image to transfer to the server
+    """
+
+    # check for invalid mode, halt connection process
+    if mode not in [0, 1]:
+        print('Invalid mode passed to make_connection')
+        return
+
+    # connect to the socket
+    host = HOST_IP # FIXME see note on global variable
     port = 4567
     sock = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
     sock.connect((host,port))
 
-    # message to send - TODO fill out
-    message = "Just a filler message for now"
+    # execute based on mode
+    if mode == 0:
+        check_in(sock)
+    elif mode == 1:
+        new_image(sock, image_data)
 
-    while True:
-        # send data
-        sock.send(message.encode('ascii'))
-
-        # received verification
-        data = sock.recv(1024)
-
-        if data == 'ACK':
-            print('Received ACK')
-        break
-
-    # close the connection
+    # done with socket, close it
     sock.close()
+
+def main():
+    # TODO implement logic for when to check-in and when to transfer an image
+
+    # DEBUG loop and set mode for testing purposes
+    while True:
+        mode = input('What mode to connect for? 123 to quit: ')
+        try:
+            mode = int(mode)
+        except ValueError:
+            print('Input was not an int!')
+            continue
+
+        if mode == 123:
+            print('Quitting...')
+            return
+
+        print('Entered client main - connecting for mode: ', mode)
+        make_connection(mode)
+    # DEBUG loop and set mode for testing purposes
+
+if __name__ == '__main__':
+    main()
--- server/TrailPiServer.py
+++ server/TrailPiServer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3.7
 import sys
 import socket
-import threading
+from threading import Thread
+
+# FIXME just put host ip up here as global for testing ease, remove when unnecessary
+HOST_IP = '::1'
 
 # enforce version
 MIN_PYTHON = (3, 7)
@@ -14,31 +17,65 @@ CLIENT_LOCK = threading.Lock()
 # helpful link for threaded python3 ipv6 server script
 # https://www.geeksforgeeks.org/socket-programming-multi-threading-python/
 
-def threadMain(cli):
+def thread_main(cli):
+    """
+    thread_main: Main logic to be performed for the clients
+    """
     while True:
-        # received from client
-        data = cli.recv(1024)
+        # receive first message from client that identifies request type
+        data = cli.recv(6)
         if not data:
             print('Closing client')
             CLIENT_LOCK.release()
             break
 
-        # send ACK to client
-        cli.send('ACK')
+        if data.lower() == 'alive':
+            print('Client is alive') # DEBUG debugging output
+            # send ACK to client
+            cli.send('ACK'.encode('ascii'))
 
-    # connection closed
+            # listen for site identification
+            data = cli.recv(2)
+            if not data:
+                # TODO handle failure to get site ID
+                print('Err getting site ID')
+                break
+            print('Site ', data, ' has check-in and is alive') # DEBUG debugging output
+            # TODO implement logic to track sites that are alive
+            cli.send('ACK'.encode('ascii'))
+
+        elif data.lower() == 'image':
+            print('Client has a new image to transfer') # DEBUG debugging output
+            # send ACK to client
+            cli.send('ACK'.encode('ascii'))
+
+            # listen for image data
+            data = cli.recv(4096)
+            if not data:
+                # TODO handle failure to get image data
+                print('Err getting image data')
+                break
+            print('Client transferred data: ', data) # DEBUG debugging output
+            cli.send('ACK'.encode('ascii'))
+
+        else:
+            # TODO handle this?
+            print('Unhandled request type')
+
+    # close connection
+    CLIENT_LOCK.release()
     cli.close()
 
-if __name__ == '__main__':
-    host = "2601:203:480:17d8:196d:2760:13fe:127e"
+def main():
+    host = HOST_IP
     port = 4567
     sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     sock.bind((host, port))
-    print("Socket bound to port ", port)
+    print('Socket bound to port ', port)
 
     # listen on socket
     sock.listen(10)
-    print("Socket listening for clients")
+    print('Socket listening for clients')
 
     # wait until client wants to exit
     while True:
@@ -50,6 +87,12 @@ if __name__ == '__main__':
         print('Connected to client: ', addr[0], ':', addr[1])
 
         # Start a new thread for client
-        start_new_thread(threadMain, (cli,))
+        thread = Thread(target = thread_main, args = (cli,))
+        thread.start()
+        thread.join()
 
     sock.close()
+
+
+if __name__ == '__main__':
+    main()
