48d7447074a0946e93c46856144e5da103ddf76d
Matthew Quesada <quesadamatthew@yahoo.com>
2019-04-06 18:22:32 -0700

    sidebar boilerplate

--- trailpi-client/src/components/sidebar/Sidebar.js
+++ trailpi-client/src/components/sidebar/Sidebar.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
 class Sidebar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    };
+    this.handleClick.bind(this);
+  }
+
+  async handleClick() {
+    const url = 'http://flask-server.wqwtbemyjw.us-west-2.elasticbeanstalk.com/TrailPiServer/api/files';
+    let response = await fetch(url);
+    let data = await response.json();
+    
+    let images = [];
+    for (let file of data.filenames) 
+      images.push(file);
+
+    this.setState({ images });
+  }
+
   render() {
+    const s3BucketURL = 'https://s3-us-west-2.amazonaws.com/trailpi-images/';
     return (
-      <div>Select photos here:</div>
+      <div>
+        <div onClick={() => this.handleClick()}>Press me!</div>
+        {this.state.images.map((imageName, key) => {
+          let imageURL = s3BucketURL + imageName;
+          return (
+            <div key={key}>
+              <img src={imageURL}/>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
