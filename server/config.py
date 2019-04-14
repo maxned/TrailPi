6a3a1ea35c4b3bcf9ca620f76b7598f669b08452
Brody Reeves <bareeves@ucdavis.edu>
2019-04-13 23:32:48 -0700

    add config file to connect to database

---
+++ server/config.py
@@ -0,0 +1,2 @@
+# format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ECS193dev!@trailpi-db-instance.ckdc802eljqg.us-west-1.rds.amazonaws.com:3306/ReserveWebcamImages'
