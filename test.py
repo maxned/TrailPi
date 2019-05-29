1a25dba7d7d9722ca5a5753bc5ab66f24aad8392
Josue Alemansanchez <josuealemansanchez@Josues-MacBook-Pro.local>
2019-05-29 03:57:09 -0700

    report created after moving images

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ from classifier import Classifier
 
 cnn = Classifier(json_file = 'model.json', weights_file = 'model.h5')
 exit_program = False
+count_true = 0
+count_false =0
 while(exit_program == False):
     type_input = input("Folder(F) or Single File(S)?: ")
     if type_input == "F" or type_input == "f":
@@ -14,14 +16,27 @@ while(exit_program == False):
         if not os.path.exists('nothing'):
             os.mkdir('nothing')
         folder_name = input("Folder Name: ")
-        test_images = os.listdir(folder_name)
-        for image in test_images:
-            print(image)
-            path_image = "./" + folder_name + "/" + image
-            if(cnn.predict_animal(path_image)):
-                os.rename(path_image, "./animals_and_humans/" + image)
+        if os.path.exists(folder_name):
+            test_images = os.listdir(folder_name)
+            if len(test_images) > 0:
+                for image in test_images:
+                    print(image)
+                    if image.startswith('.'):
+                        print(image + " not read")
+                    else:
+                        path_image = "./" + folder_name + "/" + image
+                        if(cnn.predict_animal(path_image)):
+                            os.rename(path_image, "./animals_and_humans/" + image)
+                            count_true +=1
+                        else:
+                            os.rename(path_image, "./nothing/" + image)
+                            count_false +=1
             else:
-                os.rename(path_image, "./nothing/" + image)
+                print("Folder is Empty.")
+        else:
+            print("Folder does not exists.")
+        print(f"True Motion Images Found: {count_true}")
+        print(f"False Motion Images Found: {count_false}")
         answer = input("Continue (Y/N): ")
         if answer == "N" or answer == "n":
             exit_program = True
