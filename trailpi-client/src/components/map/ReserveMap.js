6735203eb517e5f115b58b476210047036a4f4d1
Matthew Quesada <quesadamatthew@yahoo.com>
2019-06-05 22:40:44 -0700

    toggled markers based on heartbeat

--- trailpi-client/src/components/map/ReserveMap.js
+++ trailpi-client/src/components/map/ReserveMap.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Leaflet from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import { defaultLocation, mapMarkers } from '../common/mapConfig';
+import { getSiteActivity } from '../../utils/requests';
+
+const greenIcon = new Leaflet.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png'
+});
+
+const redIcon = new Leaflet.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png'
+})
 
 class ReserveMap extends React.Component {
+  constructor() {
+    super();
+    this.state = { siteActivity: [] }
+  }
+
+  async componentDidMount() {
+    let siteActivity = await getSiteActivity();
+    this.setState({ siteActivity });
+  }
+
   render() {
     const position = [defaultLocation.lat, defaultLocation.lng];
     return (
@@ -13,13 +33,33 @@ class ReserveMap extends React.Component {
           url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
         />
         {mapMarkers.map((marker, key) => {
-          return (
-            <Marker position={marker.position} key={key}>
-              <Popup>
-                <b>{marker.name}</b>
-              </Popup>
-            </Marker>
-          );
+          if (this.state.siteActivity.length === 0) {
+            return (
+              <Marker position={marker.position} key={key}>
+                <Popup>
+                  <b>{marker.name}</b>
+                </Popup>
+              </Marker>
+            );
+          }
+          if (this.state.siteActivity[key].alive === true) {
+            return (
+              <Marker position={marker.position} key={key} icon={greenIcon}>
+                <Popup>
+                  <b>{marker.name}</b>
+                </Popup>
+              </Marker>
+            );
+          }
+          else {
+            return (
+              <Marker position={marker.position} key={key} icon={redIcon}>
+                <Popup>
+                  <b>{marker.name}</b>
+                </Popup>
+              </Marker>
+            );
+          }
         })}
       </Map>
     );
